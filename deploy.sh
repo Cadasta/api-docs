caebca4d8e41e31daf8a4d715afaf9be77b91316
m-murad <murad.kuka@gmail.com>
2017-10-20 02:32:18 +0530

    1

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 
-rm -rf dddocs
-rm -rf docs-build
-mkdir docs-build
-npm install
-npm run build
-
-git clone -b gh-pages --single-branch git@github.com:Cadasta/api-docs.git dddocs
-yes | cp -rf docs-build/index.html dddocs
-yes | cp -rf docs-build/bundle.js dddocs
-yes | cp -rf css dddocs/css
-yes | cp -rf lib dddocs/lib
-
-cd dddocs
-if [[ $(git diff) || "$1" == "initial" ]]; then
-    echo "Deploying..."
-    git add .
-    git commit -m 'Deploy docs'
-    git push origin gh-pages
-else
-    echo "Nothing to deploy."
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "m-murad/api-docs" ]; then
+    echo "Just a PR."
+    exit 0
 fi
-
-echo "Cleaning up..."
-cd ..
-rm -rf dddocs
-rm -rf docs-build
-echo "Done."
