231eafbb88e781525cd36b9bd7c6afd24ae4ebc4
m-murad <murad.kuka@gmail.com>
2017-10-30 17:44:34 +0530

    Updated deploy.sh to auto-update docs when tag is created

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
-rm -rf dddocs
-rm -rf docs-build
-mkdir docs-build
-npm install
-npm run build
-
-git clone -b gh-pages --single-branch git@github.com:Cadasta/api-docs.git dddocs
-yes | cp -rf docs-build/index.html dddocs
-yes | cp -rf docs-build/bundle.js dddocs
-yes | cp -rf css dddocs/css
-yes | cp -rf lib dddocs/lib
-
-cd dddocs
-if [[ $(git diff) || "$1" == "initial" ]]; then
-    echo "Deploying..."
-    git add .
-    git commit -m 'Deploy docs'
-    git push origin gh-pages
-else
-    echo "Nothing to deploy."
+if [ -z "$TRAVIS_TAG" ]; then
+    echo "Not a TAG, skip the update."
+    exit 0
 fi
 
-echo "Cleaning up..."
-cd ..
-rm -rf dddocs
-rm -rf docs-build
-echo "Done."
+git clone --quiet --branch=gh-pages https://cadasta:$API_KEY@github.com/cadasta/api-docs  /home/travis/build/cadasta/docs-branch
+
+mkdir docs-build
+npm run build
+
+cp -rf docs-build/index.html /home/travis/build/cadasta/docs-branch
+cp -rf docs-build/bundle.js /home/travis/build/cadasta/docs-branch
+cp -rf css /home/travis/build/cadasta/docs-branch/css
+cp -rf lib /home/travis/build/cadasta/docs-branch/lib
+
+cd /home/travis/build/cadasta/docs-branch
+
+git config --global user.name "m-murad"
+git config --global user.email "murad.kuka@gmail.com"
+
+git add .
+git commit -m "Docs updated for $TRAVIS_TAG"
+git push origin gh-pages --quiet
+echo "Docs updated."
